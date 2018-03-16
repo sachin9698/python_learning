76d1c5bf16d16a8e5d7cef59af33389b6f8965f9
sachin9698 <sachin.kumar9698@gmail.com>
2018-03-16 23:57:42 +0530

    modules

---
+++ myfile1.py
@@ -0,0 +1,14 @@
+import random
+import datetime
+import os
+
+l3=['dog','cat','horse','bat','bird']
+
+ran=random.choice(l3)
+
+# print(ran)
+today=datetime.date.today()
+print(today)
+
+print(os.getcwd())
+print(os.__file__)
