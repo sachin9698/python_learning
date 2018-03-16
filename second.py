c9405be7897acdbe0b11942d86e18ba01266b915
sachin9698 <sachin.kumar9698@gmail.com>
2018-03-16 20:14:32 +0530

    more about python

---
+++ second.py
@@ -0,0 +1,79 @@
+
+# print("hello world")
+# a,b,c=1,2,3
+# print(a)
+# print(b)
+# print(c)
+
+# *************************************list*********************************
+
+# list=[1,2,"hello",3,"wow",12.4]
+# list2=[9,10]
+# print(list)
+# print(list[0])
+# print(list[2:4])
+# print(list[2:])
+# print(list * 2)
+# print(list + list2)
+
+# **********************************dictionary***********************************
+
+# dict={}
+# dict['name']='sachin'
+# dict[1]="thiw is two"
+# dict2={'name2':'jp',2:'hola'}
+#
+# print(dict['name'])
+# print(dict[1])
+# print(dict.keys())
+# print(dict.values())
+# print(dict2)
+
+
+# *******************************if-else********************************
+
+# a=5
+# if (a==1):
+#     print("this is 1")
+#     print("hola")
+# elif (a==2):
+#     print("this is 2")
+#     print("hola")
+# elif(a==3):
+#     print("this is 3")
+#     print("hola")
+# elif(a==10):
+#     print("this is 10")
+#     print("hola")
+# else:
+#     print("wrong nunber")
+
+# a=201
+# if(a<200):
+#     print("this number is less than 100")
+#     if(a>100):
+#         print("this number is greater than 100")
+# else:
+#     print("wrong number")
+
+
+# ****************************for loop*******************
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+# for num in a:
+#     print(num)
+
+# a=-3.6
+# math.ceil(a)
+# print(a)
+
+# a="hello"
+# b="l"
+# # print(a.capitalize())
+# print(a.count(b))
+
+def printme(str):
+    print(str)
+    return
+
+printme("holla jjjj")
---
+++ temp.py
@@ -0,0 +1,4 @@
+import math
+x=3.17
+math.ceil(x)
+print(x)
