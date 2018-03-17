5a26b32265387fadc0d119298105afc58ddd377a
sachin9698 <sachin.kumar9698@gmail.com>
2018-03-17 20:11:32 +0530

    added

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 # *******************************slicing in list and strings*******************
 
-mylist=[0,1,2,3,4,5,6,7,8,9]
-print(mylist[1:])
-print(mylist[8:2:-1])
-print(mylist[::-1])
+# mylist=[0,1,2,3,4,5,6,7,8,9]
+# print(mylist[1:])
+# print(mylist[8:2:-1])
+# print(mylist[::-1])
+#
+# stri='www.google.com'
+# print(stri[-4:])
+# print(stri[4:])
+# print(stri[4:-4])
 
-stri='www.google.com'
-print(stri[-4:])
-print(stri[4:])
-print(stri[4:-4])
+
+# ************************input and output**********************************
+
+# name=input("enter the name :")
+# print("hello",name)
+
+# num1=int(input("enter 1st number :"))
+# num2=int(input("enter 2nd number :"))
+# x=num1//num2
+# y=num1%num2
+# print(num1+num2)
+# print("the quotient of",num1,"and",num2,"is",x,"and remainder is",y)
+
+# person = input('Enter your name: ')
+# greeting = 'Hello, {}!'.format(person)
+# print(greeting)
+
+# person = input('Enter your name: ')
+# print('Hello {}!'.format(person))
+
+# a="hello"
+# b="sachin"
+# c="how are you"
+# print("{} {} {}".format(a,b,c))
+
+a = 5
+b = 9
+setStr = 'The set is {{{}, {}}}.'.format(a, b)
+print(setStr)
