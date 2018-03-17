072ee09c1c1d9336ea86685d5ef1c52a6675d58b
sachin9698 <sachin.kumar9698@gmail.com>
2018-03-17 12:39:11 +0530

    slicing

---
+++ main.py
@@ -0,0 +1,11 @@
+# *******************************slicing in list and strings*******************
+
+mylist=[0,1,2,3,4,5,6,7,8,9]
+print(mylist[1:])
+print(mylist[8:2:-1])
+print(mylist[::-1])
+
+stri='www.google.com'
+print(stri[-4:])
+print(stri[4:])
+print(stri[4:-4])
