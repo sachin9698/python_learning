331264abef1cb1e8d7000fd9344648ad69a44670
sachin9698 <sachin.kumar9698@gmail.com>
2018-03-22 23:06:17 +0530

    generators

--- main2.py
+++ main2.py
@@ -12,11 +12,60 @@
 
 #***********************iterator in for loop**********************
 
+
 # lis=[1,2,3,4,5]
 #
 # for element in lis:
 #     print(element)
 
-int()
-inf=iter(int, sentinel=1)
-next(inf)
+
+#************************generators*********************************
+
+# def my_gen():
+#     n=1;
+#     print("this is printed first !")
+#     yield n
+#
+#     n=n+1
+#     print("this is printed second !")
+#     yield n
+#
+#     n=n+1
+#     print("this is printed second !")
+#     yield n
+
+# a=my_gen()
+# next(a)
+# next(a)
+# next(a)
+
+#uisng for loop
+
+# for elem in my_gen():
+#     print(elem)
+
+
+#eg-2
+
+# def str_rev(str):
+#     l=len(str)
+#     for i in range(l-1,-1,-1):
+#         yield str[i]
+#
+# for char in str_rev("hello"):
+#     print(char)
+
+#eg-3
+
+def pow(max=0):
+    n=0
+    while n<max:
+        yield 2**n
+        n=n+1
+
+a=pow(max=5)
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
